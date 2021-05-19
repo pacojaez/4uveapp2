010ba6e5290b7293b0af8d0c26533b0a46cb2974
Paco Jaez <pacojaez@gmail.com>
2021-05-19 15:47:46 +0200

    arreglado gustnavbar

--- resources/views/livewire/guest-navbar.blade.php
+++ resources/views/livewire/guest-navbar.blade.php
@@ -1,36 +1,36 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex w-full">
                 <!-- Logo -->
-                <div class="flex-shrink-0 flex items-center">
+                <div class="flex items-center flex-shrink-0">
                     <a href="{{ route('welcome') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        <x-jet-application-mark class="block w-auto h-9" />
                     </a>
                     <!-- BUSCADOR -->
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <input class="max-w-md sm:max-w-1/2 h-5/6 px-3 rounded ml-20 mt-2 mb-2 focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg" type="search" placeholder="Buscar Productos...">
+                    <input class="max-w-md px-3 mt-2 mb-2 ml-20 text-xl rounded shadow-lg sm:max-w-1/2 h-5/6 focus:outline-none focus:shadow-outline" type="search" placeholder="Buscar Productos...">
                     </div>
-                     <x-jet-nav-link href="{{ route('allproducts') }}" class="font-bold m-5">
+                     <x-jet-nav-link href="{{ route('allproducts') }}" class="m-5 font-bold">
                         {{ __('All Products') }}
                     </x-jet-nav-link>
                 </div>
                 @if(Auth::user())
-                <div class="sm:flex flex-grow sm:items-center sm:ml-6 place-items-center justify-end">    
+                <div class="justify-end flex-grow sm:flex sm:items-center sm:ml-6 place-items-center">
                     <!-- Settings Dropdown -->
-                    <div class="ml-3 relative">
+                    <div class="relative ml-3">
                         <x-jet-dropdown align="right" width="48">
                             <x-slot name="trigger">
                                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                    <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                        <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                    <button class="flex text-sm transition border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300">
+                                        <img class="object-cover w-8 h-8 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                                     </button>
                                 @else
                                     <span class="inline-flex rounded-md">
-                                        <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
+                                        <button type="button" class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition bg-white border border-transparent rounded-md hover:text-gray-700 focus:outline-none">
                                             Bienvenido, {{ Auth::user()->name }}
-    
+
                                             <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                             </svg>
@@ -38,29 +38,29 @@
                                     </span>
                                 @endif
                             </x-slot>
-    
+
                             <x-slot name="content">
                                 <!-- Account Management -->
                                 <div class="block px-4 py-2 text-xs text-gray-400">
                                     {{ __('Manage Account') }}
                                 </div>
-    
+
                                 <x-jet-dropdown-link href="{{ route('profile.show') }}">
                                     {{ __('Profile') }}
                                 </x-jet-dropdown-link>
-    
+
                                 @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                                     <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
                                         {{ __('API Tokens') }}
                                     </x-jet-dropdown-link>
                                 @endif
-    
+
                                 <div class="border-t border-gray-100"></div>
-    
+
                                 <!-- Authentication -->
                                 <form method="POST" action="{{ route('logout') }}">
                                     @csrf
-    
+
                                     <x-jet-dropdown-link href="{{ route('logout') }}"
                                              onclick="event.preventDefault();
                                                     this.closest('form').submit();">
@@ -72,11 +72,11 @@
                     </div>
                 </div>
                 @else
-                <div class="flex-grow flex items-center justify-end">
-                    <x-jet-nav-link href="{{ route('login') }}" class="font-bold m-5">
+                <div class="flex items-center justify-end flex-grow">
+                    <x-jet-nav-link href="{{ route('login') }}" class="m-5 font-bold">
                         {{ __('Login') }}
                     </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('register') }}" class="font-bold m-5">
+                    <x-jet-nav-link href="{{ route('register') }}" class="m-5 font-bold">
                         {{ __('Register') }}
                     </x-jet-nav-link>
                 </div>
@@ -84,9 +84,9 @@
             </div>
 
             <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+            <div class="flex items-center -mr-2 sm:hidden">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 text-gray-400 transition rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500">
+                    <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
@@ -98,22 +98,22 @@
     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
         @livewire('categories-navbar')
     </div>
-    
+
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <!-- Responsive Settings Options -->
         <!-- BUSCADOR -->
-        <div class="pt-2 pb-3 space-y-1 flex items-center">
-            <input class="max-w-md sm:max-w-1/2 h-5/6 px-3 rounded ml-20 mt-2 mb-2 focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg" type="search" placeholder="Buscar Productos...">
+        <div class="flex items-center pt-2 pb-3 space-y-1">
+            <input class="max-w-md px-3 mt-2 mb-2 ml-20 text-xl rounded shadow-lg sm:max-w-1/2 h-5/6 focus:outline-none focus:shadow-outline" type="search" placeholder="Buscar Productos...">
             </div>
          <!-- Responsive Settings Options Menu Principal -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
+        <div class="pt-4 pb-1 border-gray-200 border-t-1">
             <div class="flex items-center px-4">
                 <div class="pt-2 pb-3 space-y-1">
                     @livewire('categories-navbar')
                 </div>
             </div>
-        </div> 
+        </div>
     </div>
 </nav>
