ac0bdc0185da29a8038aeb7986d7754d6b03c9e4
Paco Jaez <pacojaez@gmail.com>
2021-07-03 19:17:39 +0200

    solved footer responsive

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full mb-0 bg-gray-600">
-    <div class="flex flex-row justify-center px-10 py-4 text-xl text-yellow-100 font-title">
+    <div class="justify-center px-10 py-4 text-xl text-yellow-100 md:flex md:flex-row font-title">
         <div class="mx-4 pointer hover:underline hover:text-red-400 font-subtitles">Quienes Somos</div>
         <div class="mx-4 pointer hover:underline hover:text-red-400 font-subtitles"><a href="{{ route( 'avisolegal' ) }}">Aviso Legal </a></div>
         <div class="mx-4 pointer hover:underline hover:text-red-400 font-subtitles"><a href="{{ route( 'politicacookies' ) }}">Política de Cookies</a></div>
