b23778c1f97fc0ca6c44cc3e190bf4b7daebe292
Paco Jaez <pacojaez@gmail.com>
2021-07-12 13:13:01 +0200

    SinglProductCard not used in the project

--- app/Http/Livewire/SingleProductCard.php
+++ app/Http/Livewire/SingleProductCard.php
@@ -11,24 +11,27 @@ use App\Models\Subcategorie;
 use App\Models\Porte;
 
 use App\Models\Oferta;
-// use App\Models\Cart;
+
+
+/**
+ * CLASS NOT USED IN THIS PROJECT
+ * WE USE THE OFFER CLASS TO SHOW TO THE USER
+ */
 
 class SingleProductCard extends Component
 {
-
+    /**
+     * Public properties to pass to the view
+     */
     public Product $product;
     public Subcategorie $subcategorie;
     public Categorie $category;
     public Porte $porte;
     public $quantity = 1;
     public Oferta $oferta;
-    // public $cart;
-
-
 
     public function mount($id){
-        dd($id);
-        // $this->set();
+
         $this->quantity;
         $this->oferta = Oferta::findOrFail($id);
         $this->product = Product::where('id', 'like', $this->oferta->product_id);
@@ -36,12 +39,10 @@ class SingleProductCard extends Component
         $this->category = Categorie::where('id', '=', $this->subcategorie->category_id)->first()->name;
         $this->porte = $this->product->porte;
 
-        dd($this->category->name);
     }
 
     public function render(): View
     {
-        // $this->product = Product::findOrFail($id);
         return view('livewire.single-product-card', [
             'oferta' => $this->oferta,
             'product' => $this->product,
@@ -49,7 +50,6 @@ class SingleProductCard extends Component
             'porte' => $this->porte,
             'quantity' => $this->quantity,
             'category' => $this->category,
-            // 'cart' => $this->cart
             ]);
     }
 
@@ -61,11 +61,7 @@ class SingleProductCard extends Component
     public function addToCart(): void
     {
         Cart::add($this->product->id, $this->product->name, $this->product->getRawOriginal('invoice_cost_price'), $this->quantity);
-        // dd($one);
-        // $this->emitUp('productAddedToCart');
-        // $this->emitTo('guest-nav-bar', 'productAdded');
-        // $this->emitTo('cart', 'productAdded');
+
         $this->emitTo('nav-cart', 'refresh');
-        // dd($this->quantity);
     }
 }
