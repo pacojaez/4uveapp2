2fe709cca861b8edbbac94a71e3858db90bde264
Paco Jaez <pacojaez@gmail.com>
2021-07-12 09:04:19 +0200

    clean view cart

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -7,7 +7,6 @@
         @endif
     </div>
     <div class="max-w-full p-5 mx-2 my-2 border-2 rounded">
-        {{-- @if ($content->count() > 0) --}}
         @if( count($content) > 0 )
         <table class="min-w-full divide-y divide-gray-200">
             <thead class="bg-gray-50">
@@ -109,82 +108,9 @@
             obtenido para el envio!</p>
         <p class="mb-2 text-2xl font-light text-center">Muchas Gracias por confiar en 4uve!</p>
         @else
-        {{-- <p class="mb-2 text-2xl text-center">Hemos vaciado tu carrito. </p> --}}
         <p class="mb-2 text-2xl font-light text-center">Muchas Gracias por tu interés en 4uve!</p>
         @endif
         @endif
     </div>
-    {{-- <script>
-            window.addEventListener('refresh-page', event => {
-               window.location.reload(true);
-            })
-          </script> --}}
 </div>
-{{-- <div>
-        <div class="w-5/6 mx-auto">
-            <div class="my-6 bg-white rounded shadow-md">
-                @if( count($cart['products']) > 0)
-                <table class="w-full text-left border-collapse">
-                    <thead>
-                        <tr>
-                            <th class="px-6 py-4 text-sm font-bold uppercase border-b bg-grey-lightest text-grey-dark border-grey-light">
-                                PRODUCTO
-                            </th>
-                            <th class="px-6 py-4 text-sm font-bold uppercase border-b bg-grey-lightest text-grey-dark border-grey-light">
-                                CODIGO PRODUCTO
-                            </th>
-                            <th class="px-6 py-4 text-sm font-bold uppercase border-b bg-grey-lightest text-grey-dark border-grey-light">
-                                DIMENSIONES
-                            </th>
-                            <th class="px-6 py-4 text-sm font-bold uppercase border-b bg-grey-lightest text-grey-dark border-grey-light">
-                                PESO
-                            </th>
-                            <th class="px-6 py-4 text-sm font-bold uppercase border-b bg-grey-lightest text-grey-dark border-grey-light">
-                                PRECIO
-                            </th>
-                            <th class="px-6 py-4 text-sm font-bold uppercase border-b bg-grey-lightest text-grey-dark border-grey-light">
-                                Actions
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($cart['products'] as $product)
-                        <tr class="hover:bg-grey-lighter">
-                            <td class="px-6 py-4 border-b border-grey-light">
-                                <span class="w-20 h-20">
-                                    <img alt="{{ $product->name }}" class="object-cover object-center w-20 h-20"
-src="{{asset('storage/images/products/'.$product->product_image)}}" />
-</span>
-{{ $product->name }}
-</td>
-<td class="px-6 py-4 border-b border-grey-light">{{ $product->EAN13_individual }}</td>
-<td class="px-6 py-4 border-b border-grey-light">{{ $product->dimensions_boxes }} mm </td>
-<td class="px-6 py-4 border-b border-grey-light">
-    <span>Unidades: {{ $product->pack_units }}</span>
-    <br>
-    Peso: {{ $product->weight }} Kgs
-    <br>
-    Dimesiones: {{ $product->dimensions_boxes }} mm
-</td>
-<td class="px-6 py-4 border-b border-grey-light"> {{ $product->invoice_cost_price }} € </td>
-<td class="px-6 py-4 border-b border-grey-light">
-    <a wire:click="removeFromCart({{ $product->id }})"
-        class="px-3 py-1 text-xs font-bold text-green-600 rounded cursor-pointer bg-green hover:bg-green-dark">Remove</a>
-</td>
-</tr>
-@endforeach
-</tbody>
-</table>
-<div class="w-full p-6 text-right">
-    <button wire:click="checkout()" class="px-4 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-700">
-        Checkout
-    </button>
-</div>
-@else
-<div class="w-full h-full p-6 text-center border-collapse">
-    <span class="text-lg">¡TU CARRITO ESTÁ VACIO!</span>
-</div>
-@endif
-</div>
-</div>
-</div> --}}
+
